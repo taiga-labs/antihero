065045e6e919f472c67c3911ec3699f89c5d4b34
hifided <hifided@gmail.com>
2024-03-23 19:11:17 +0300

    for test

--- src/web/app/routes.py
+++ src/web/app/routes.py
@@ -1,157 +1,27 @@
-import hmac
-import hashlib
-import json
-from urllib import parse
-
-from aiogram import Bot, types
 from aiohttp import web
-
-from settings import settings
-from src.storage.driver import get_redis_async_client
-from src.storage.schemas import GameState
-from src.web import web_logger
-
 
 router = web.RouteTableDef()
 
 
 @router.post("/auth")
 async def auth(request):
-    data = await request.json()
-    data_check_string = parse.unquote(data["data_check_string"])
-    hash = data["hash"]
-
-    secret_key = hmac.new(
-        key="WebAppData".encode(),
-        msg=settings.TELEGRAM_API_KEY.get_secret_value().encode(),
-        digestmod=hashlib.sha256,
-    )
-
-    hash_check = hmac.new(
-        key=secret_key.digest(),
-        msg=data_check_string.encode(),
-        digestmod=hashlib.sha256,
-    )
-
-    if hash_check.hexdigest() == hash:
-        return web.Response(status=200)
-    return web.Response(status=403)
+    return web.Response(status=200)
 
 
 @router.post("/preinfo")
 async def preinfo(request):
-    redis_session = await get_redis_async_client(url=settings.GAME_BROKER_URL)
-    data = await request.json()
-    game_uuid = data["uuid"]
-    player_id = int(data["player_id"])
-
-    if not await redis_session.exists(game_uuid):
-        web_logger.info(
-            f"preinfo | game ({game_uuid}) : player ({player_id}) | access denied | game closed"
-        )
-        return web.Response(status=403, text="Игра завершена")
-
-    game_state_raw = await redis_session.get(name=game_uuid)
-    game_state = GameState.model_validate_json(game_state_raw)
-
-    player = (
-        game_state.player_l
-        if player_id == game_state.player_l.player_id
-        else game_state.player_r
-    )
-
-    await redis_session.close()
     body = {
-        "score": player.score,
-        "attempts": player.attempts,
+        "score": 0,
+        "attempts": 0,
     }
     return web.json_response(body)
 
 
 @router.post("/start")
 async def start(request):
-    redis_session = await get_redis_async_client(url=settings.GAME_BROKER_URL)
-
-    data = await request.json()
-    game_uuid = data["uuid"]
-    player_id = int(data["player_id"])
-
-    if not await redis_session.exists(game_uuid):
-        web_logger.info(
-            f"start | game ({game_uuid}) : player ({player_id}) | access denied | game closed"
-        )
-        return web.Response(status=403, text="Игра завершена")
-
-    game_state_raw = await redis_session.get(name=game_uuid)
-    game_state = GameState.model_validate_json(game_state_raw)
-
-    player = (
-        game_state.player_l
-        if player_id == game_state.player_l.player_id
-        else game_state.player_r
-    )
-
-    if player.attempts == 0:
-        web_logger.info(
-            f"start | game ({game_uuid}) : player ({player_id}) | access denied | attemption limit"
-        )
-        return web.Response(status=403, text="Исчерпано количество попыток")
-
-    web_logger.info(f"start | game ({game_uuid}) : player ({player_id}) | start game")
-
-    if player_id == game_state.player_l.player_id:
-        game_state.player_l.attempts = game_state.player_l.attempts - 1
-    else:
-        game_state.player_r.attempts = game_state.player_r.attempts - 1
-    await redis_session.set(name=game_uuid, value=json.dumps(game_state.model_dump()))
-
-    await redis_session.close()
     return web.Response(status=200)
 
 
 @router.post("/score")
 async def score(request):
-    redis_session = await get_redis_async_client(url=settings.GAME_BROKER_URL)
-
-    bot: Bot = request.app["bot"]
-
-    data = await request.json()
-    query_id = data["query_id"]
-    game_uuid = data["uuid"]
-    player_id = int(data["player_id"])
-    score = int(data["score"])
-
-    if not await redis_session.exists(game_uuid):
-        web_logger.info(
-            f"score | game ({game_uuid}) : player ({player_id}) | access denied | game closed"
-        )
-        return web.Response(status=403, text="Игра завершена")
-
-    game_state_raw = await redis_session.get(name=game_uuid)
-    game_state = GameState.model_validate_json(game_state_raw)
-
-    if player_id == game_state.player_l.player_id:
-        game_state.player_l.score += score
-        total_score = game_state.player_l.score
-        attempts = game_state.player_l.attempts
-    else:
-        game_state.player_r.score += score
-        total_score = game_state.player_r.score
-        attempts = game_state.player_r.attempts
-
-    await redis_session.set(name=game_uuid, value=json.dumps(game_state.model_dump()))
-
-    if not attempts:
-        exit_text = (
-            f"Игра #{game_uuid.rsplit('-', 1)[-1]}\nОбщий счет: {total_score}\n\n"
-            f"Ожидание результатов соперника..."
-        )
-        result = types.InlineQueryResultArticle(
-            id=query_id,
-            title="Score",
-            input_message_content=types.InputTextMessageContent(message_text=exit_text),
-        )
-        await bot.answer_web_app_query(web_app_query_id=query_id, result=result)
-
-    await redis_session.close()
     return web.Response(status=200)
