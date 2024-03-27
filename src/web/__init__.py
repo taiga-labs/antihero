9667ee4db1fa1f4dce981682d4a741f4ad751cc8
hifided <hifided@gmail.com>
2024-03-27 22:42:27 +0300

    test route

--- src/web/__init__.py
+++ src/web/__init__.py
@@ -1,7 +1,7 @@
 import logging
 
-from flask import Flask
-from flask_cors import CORS
+from flask import Flask, Response
+from flask_cors import CORS, cross_origin
 from flask_socketio import SocketIO
 
 from src.bot.factories import bot
@@ -10,10 +10,28 @@ from src.bot.factories import bot
 app = Flask(__name__)
 app.config["bot"] = bot
 
+# cors = CORS(app, resources={r"*": {"origins": "*"}})
+# cors = CORS(app, resources={r"/*": {"origins": "*"}})
 cors = CORS(app)
 app.config["CORS_HEADERS"] = "Content-Type"
 
-socketio = SocketIO(app)
+
+@app.route("/", methods=["OPTIONS", "GET"])
+# @cross_origin()
+def start():
+    response = Response(status=200)
+    return response
+
+
+# @app.after_request
+# def after_request(response):
+#     response.headers.add("Access-Control-Allow-Origin", "*")
+#     response.headers.add("Access-Control-Allow-Headers", "Content-Type,Authorization")
+#     response.headers.add("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE")
+#    return response
+
+
+# socketio = SocketIO(app)
 
 logging.basicConfig()
 web_logger = logging.getLogger("ANTIHERO_WEB")
--- src/web/__main__.py
+++ src/web/__main__.py
@@ -1,4 +1,4 @@
 from settings import settings
 from src.web import app
 
-app.run(debug=True, host="0.0.0.0", port=settings.MINIAPP_PORT)
+app.run(debug=False, host="0.0.0.0", port=settings.MINIAPP_PORT)
