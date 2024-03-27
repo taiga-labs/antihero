0d1199a1561ef49f9a502a119332d0ffb0e86aeb
hifided <hifided@gmail.com>
2024-03-27 13:51:06 +0300

    fix: deletee events redis

--- src/web/app/events.py
+++ src/web/app/events.py
@@ -1,7 +1,3 @@
-import json
-from settings import settings
-from src.storage.driver import get_redis_async_client
-from src.storage.schemas import GameState, GameConnection
 from src.web import web_logger, sio
 
 
@@ -13,57 +9,8 @@ async def connect(sid, data):
 @sio.on("disconnect")
 async def disconnect(sid):
     web_logger.info(f"disconnect | socket connection closed | sid: {sid}")
-    redis_game_session = await get_redis_async_client(url=settings.GAME_BROKER_URL)
-    redis_connection_session = await get_redis_async_client(
-        url=settings.GAME_CONNECTION_BROKER_URL
-    )
-
-    game_connection_raw = await redis_connection_session.get(name=sid)
-    if game_connection_raw:
-        game_connection = GameConnection.model_validate_json(game_connection_raw)
-
-        game_state_raw = await redis_game_session.get(name=game_connection.uuid)
-        game_state = GameState.model_validate_json(game_state_raw)
-
-        if game_connection.player_id == game_state.player_l.player_id:
-            game_state.player_l.sid = None
-        else:
-            game_state.player_r.sid = None
-
-        await redis_game_session.set(
-            name=game_connection.uuid, value=json.dumps(game_state.model_dump())
-        )
-        await redis_connection_session.delete(sid)
-    await redis_game_session.close()
-    await redis_connection_session.close()
 
 
 @sio.on("set_connection")
 async def set_connection(sid, data):
     web_logger.info(f"set_connection | socket set connection data | sid: {sid}")
-    redis_game_session = await get_redis_async_client(url=settings.GAME_BROKER_URL)
-    redis_connection_session = await get_redis_async_client(
-        url=settings.GAME_CONNECTION_BROKER_URL
-    )
-
-    game_connection = GameConnection(
-        player_id=data["player_id"],
-        uuid=data["uuid"],
-    )
-    await redis_connection_session.set(
-        name=sid, value=json.dumps(game_connection.model_dump())
-    )
-
-    game_state_raw = await redis_game_session.get(name=game_connection.uuid)
-    game_state = GameState.model_validate_json(game_state_raw)
-
-    if game_connection.player_id == game_state.player_l.player_id:
-        game_state.player_l.sid = sid
-    else:
-        game_state.player_r.sid = sid
-
-    await redis_game_session.set(
-        name=game_connection.uuid, value=json.dumps(game_state.model_dump())
-    )
-    await redis_game_session.close()
-    await redis_connection_session.close()
--- src/web/app/routes.py
+++ src/web/app/routes.py
@@ -5,11 +5,23 @@ router = web.RouteTableDef()
 
 @router.post("/auth")
 async def auth(request):
+    try:
+        data = await request.json()
+        d = data["data_check_string"]
+        hash = data["hash"]
+    except:
+        return web.Response(status=403)
     return web.Response(status=200)
 
 
 @router.post("/preinfo")
 async def preinfo(request):
+    try:
+        data = await request.json()
+        game_uuid = data["uuid"]
+        player_id = int(data["player_id"])
+    except:
+        return web.Response(status=403)
     body = {
         "score": 0,
         "attempts": 0,
@@ -19,9 +31,23 @@ async def preinfo(request):
 
 @router.post("/start")
 async def start(request):
+    try:
+        data = await request.json()
+        game_uuid = data["uuid"]
+        player_id = int(data["player_id"])
+    except:
+        return web.Response(status=403)
     return web.Response(status=200)
 
 
 @router.post("/score")
 async def score(request):
+    try:
+        data = await request.json()
+        query_id = data["query_id"]
+        game_uuid = data["uuid"]
+        player_id = int(data["player_id"])
+        score = int(data["score"])
+    except:
+        return web.Response(status=403)
     return web.Response(status=200)
