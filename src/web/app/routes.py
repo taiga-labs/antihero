006d828351ed9c6feaf86e2d48eff0882729bfb6
hifided <hifided@gmail.com>
2024-04-14 22:06:59 +0300

    upd: yield session local

--- src/web/app/routes.py
+++ src/web/app/routes.py
@@ -7,8 +7,12 @@ from src.storage.driver import async_session
 router = APIRouter()
 
 
-@router.get("/game_status/{uuid}")
-async def game_status(uuid: str, db_session: AsyncSession = Depends(async_session)):
+def get_session_local():
+    yield async_session()
+
+
+@router.get("/game_status")
+async def game_status(uuid: str, db_session: AsyncSession = Depends(get_session_local)):
     game_dao = GameDAO(db_session)
     is_c = await game_dao.is_closed(uuid=uuid)
     await db_session.close()
